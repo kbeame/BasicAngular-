50cf5c1bcd59a58c644994e11c556bc12a7b0266
Katherine Beame <katherine.beame@gmail.com>
2016-05-10 10:20:49 -0700

    added basic structure and webpack

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const webpack = require('webpack-stream');
+
+gulp.task('webpack', () => {
+  gulp.src('./app/js/entry.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('static', () => {
+  gulp.src('app/**/*.html')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('default', ['webpack', 'static']);
